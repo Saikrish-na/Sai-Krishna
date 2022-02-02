f4dbe28fe6cd778855bef5fd5acc440094892cbe
saik5 <saik5@DESKTOP-I0BEPKT>
2022-02-02 09:03:29 +0530

    Escape Sequence

---
+++ Java/src/aspire/EscapeSequenceDemo.java
@@ -0,0 +1,11 @@
+package aspire;
+
+public class EscapeSequenceDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello\"World\"");
+		System.out.println("Hello \n World \n\n Aspire");
+		System.out.println("Hello \n\t World \n\t\t Aspire");
+	}
+
+}
